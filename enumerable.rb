202c3d4b7073228c3b61eb0b8026e55beb8c2a11
ian <ianfarr44@gmail.com>
2021-11-08 14:59:55 -0800

    aaded my_select

--- enumerable.rb
+++ enumerable.rb
@@ -3,14 +3,34 @@ class Array
   def my_each(&block)
     i = 0
     while i < self.length
-      block.call(i)
+      block.call(self[i])
       i += 1
     end
     self
   end
 
+  def my_select(&prc)
+
+    arr = []
+
+    self.my_each do |ele|
+
+        if prc.call(ele)
+            arr << ele
+        end
+
+    end
+
+    arr
+  end
+
+
 end
 
-return_value = [1,2,3]
-p return_value.my_each {|x| x * 2}
+# return_value = [1,2,3]
+# p return_value.my_each {|x| x * 2}
 
+
+a = [1, 2, 3]
+p a.my_select { |num| num > 1 } # => [2, 3]
+p a.my_select { |num| num == 4 } # => []
