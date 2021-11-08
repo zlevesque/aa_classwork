91ef0e6f27dea9397d26e738604191727fe205e9
Zyaire <89179829+zlevesque@users.noreply.github.com>
2021-11-08 14:49:06 -0800

    my_each complete

--- enumerable.rb
+++ enumerable.rb
@@ -1,2 +1,16 @@
-puts "first puts run"
-puts "hi"
+class Array
+
+  def my_each(&block)
+    i = 0
+    while i < self.length
+      block.call(i)
+      i += 1
+    end
+    self
+  end
+
+end
+
+return_value = [1,2,3]
+p return_value.my_each {|x| x * 2}
+
