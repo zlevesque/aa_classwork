9beea8cd2044558dd899fdc3f4ec1f5b5f6516c1
Zyaire <89179829+zlevesque@users.noreply.github.com>
2021-11-08 16:03:12 -0800

    flatten done

--- enumerable.rb
+++ enumerable.rb
@@ -1,3 +1,4 @@
+require 'byebug'
 class Array
 
   def my_each(&block)
@@ -24,6 +25,63 @@ class Array
     arr
   end
 
+  def my_reject(&block)
+    
+    arr = []
+
+    self.my_each do |ele|
+
+      if !block.call(ele)
+        arr << ele
+      end
+
+    end
+
+    arr
+  end
+
+  def my_any?(&block)
+
+    self.my_each do |ele|
+
+      return true if block.call(ele)
+      
+    end
+
+    false
+
+  end
+
+  def my_all?(&block)
+
+    self.my_each do |ele|
+
+      return false if !block.call(ele)
+      
+    end
+
+    true
+
+  end
+
+  def my_flatten
+    arr = []
+    self.each do |ele|
+
+      if !ele.is_a?(Array)
+
+        arr << ele 
+
+      else
+
+        arr += ele.my_flatten
+
+      end
+
+    end
+
+    arr
+  end
 
 end
 
@@ -31,6 +89,19 @@ end
 # p return_value.my_each {|x| x * 2}
 
 
-a = [1, 2, 3]
-p a.my_select { |num| num > 1 } # => [2, 3]
-p a.my_select { |num| num == 4 } # => []
+# a = [1, 2, 3]
+# p a.my_select { |num| num > 1 } # => [2, 3]
+# p a.my_select { |num| num == 4 } # => []
+
+# a = [1, 2, 3]
+# p a.my_reject { |num| num > 1 } # => [1]
+# p a.my_reject { |num| num == 4 } # => [1, 2, 3]
+
+# a = [1, 2, 3]
+# p a.my_any? { |num| num > 1 } # => true
+# p a.my_any? { |num| num == 4 } # => false
+# p a.my_all? { |num| num > 1 } # => false
+# p a.my_all? { |num| num < 4 } # => true
+
+p [1, 2, 3, [4, [5, 6]], [[[7]], 8]].my_flatten # => [1, 2, 3, 4, 5, 6, 7, 8]
+#p [1,2,3].my_flatten
