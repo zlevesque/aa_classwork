4d2f3c247abffcbbd7a92f1653f104adb0915c28
Zyaire <89179829+zlevesque@users.noreply.github.com>
2021-11-12 14:09:30 -0800

    Start Phase 2

--- Week-3/day-5/skeleton/lib/00_tree_node.rb
+++ Week-3/day-5/skeleton/lib/00_tree_node.rb
@@ -1,6 +1,17 @@
 #intro tree node
 class PolyTreeNode
 
-  
+  def initialize(value)
+    @parent = nil
+    @children = []
+    @value = value
+  end
+
+  def parent=(node)
+    @parent = node
+    self.children.push(node)
+  end
+
+  attr_reader :parent, :children, :value
 
 end
