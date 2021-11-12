ac7592a7e97ee4cda3aa4539abfad3cea249e3c4
Zyaire <89179829+zlevesque@users.noreply.github.com>
2021-11-12 15:36:51 -0800

    Phase 1 to 3 complete

--- Week-3/day-5/skeleton/lib/00_tree_node.rb
+++ Week-3/day-5/skeleton/lib/00_tree_node.rb
@@ -10,19 +10,46 @@ class PolyTreeNode
 
   #should add the child node to the passed node's children
 
-  def parent=(node) 
+  def parent=(new_parent) 
     # debugger
-    return if node == parent 
-    @parent = node
-    node.children.push(self) 
-    @parent = nil if parent == nil
-    # node.parent.children.shift(node)
-  end
+    return if new_parent == parent
 
-  def add_child
+    if self.parent != nil
+      self.parent.children.delete(self)
+    end
+
+    @parent = new_parent
+
+    return nil if parent == nil
+
+    new_parent.children.push(self)
     
   end
 
+  def add_child(new_child) # new_child = ARRAY
+    new_child.parent = self
+  end
+
+  def remove_child(new_child)
+    return raise "Node is not a child" if new_child.parent == nil
+    new_child.parent = nil
+  end
+
+
+
   attr_reader :parent, :children, :value
+
+end
+
+class Searchable
+
+  def dfs(value)
+    
+  end
+
+
+
+
+
 
 end
