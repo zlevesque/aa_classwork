f00e0d563520ae460136e4792c57179e25b572b3
Zyaire <89179829+zlevesque@users.noreply.github.com>
2021-11-16 13:55:38 -0800

    Started Board and Pieces

--- Week-4/Day-2/chess/board.rb
+++ Week-4/Day-2/chess/board.rb
@@ -1,1 +1,28 @@
-# game.rb
+# Board Class
+class Board
+  attr_reader :board, #:row
+  def initialize
+    @board = Array.new(8) {Array.new(8, nil)}
+    @size = (8 * 8)
+    #@row = @board.each {|row| }
+  end
+  
+  def [](pos)
+    row, col = pos
+    @board[row][col]
+  end
+
+  def []=(pos,val)
+    row, col = pos
+    @board[row][col] = val
+  end
+
+  def move_piece(start_pos, end_pos)
+    if start_pos != nil && end_pos == nil
+      
+    else
+      raise "ERROR - move piece"
+    end
+  end
+  
+end
