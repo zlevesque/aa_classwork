dd86ff5b56858d948c3e1b5d2bc0c7b6af5689d2
Justin Stith <justinbluejstith@gmail.com>
2021-11-16 17:47:01 -0500

    add movepiece

--- Week-4/Day-2/chess/board.rb
+++ Week-4/Day-2/chess/board.rb
@@ -1,8 +1,8 @@
 # Board Class
 class Board
-  attr_reader :board, #:row
+  attr_reader :board #:row
   def initialize
-    @board = Array.new(8) {Array.new(8, nil)}
+    @board = Array.new(8) {Array.new(8, '_')}
     @size = (8 * 8)
     #@row = @board.each {|row| }
   end
@@ -18,11 +18,25 @@ class Board
   end
 
   def move_piece(start_pos, end_pos)
-    if start_pos != nil && end_pos == nil
-      
+    # check piece's color 
+    # valid moves are unique to different pieces
+    if self[start_pos] != '_' 
+      self[start_pos] =  'cat' # come fix me pls. value is placeholder for now
     else
       raise "ERROR - move piece"
     end
   end
+
+  # initialize board, populate it and take a look
+    def populate_board
+        
+    end
+  
+  
   
 end
+
+b= Board.new
+p b.board
+p b.move_piece([0,0],[1,2])
+p b.board
