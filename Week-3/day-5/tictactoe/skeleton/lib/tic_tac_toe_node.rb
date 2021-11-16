44c6ad56a1c0f2c6082deffa06d970a2d1d917ca
Zyaire <89179829+zlevesque@users.noreply.github.com>
2021-11-15 17:28:46 -0800

    Start TTT

--- Week-3/day-5/tictactoe/skeleton/lib/tic_tac_toe_node.rb
+++ Week-3/day-5/tictactoe/skeleton/lib/tic_tac_toe_node.rb
@@ -1,8 +1,16 @@
 require_relative 'tic_tac_toe'
 
 class TicTacToeNode
+  attr_reader :board, :next_mover_mark, :prev_move_pos
+
   def initialize(board, next_mover_mark, prev_move_pos = nil)
+    @board = board
+    @next_mover_mark = next_mover_mark
+    @prev_mov_pos = prev_move_pos
+    #@node = TicTacToeNode.new(board, next_mover_mark)
   end
+
+
 
   def losing_node?(evaluator)
   end
@@ -13,5 +21,12 @@ class TicTacToeNode
   # This method generates an array of all moves that can be made after
   # the current move.
   def children
+    (0...@board.length).each do |row|
+      (0...@board.length).each do |col|
+        if row, col != mark
+          
+        end
+      end
+    end
   end
 end
